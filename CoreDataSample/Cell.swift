54af084be4137e14846728c446c41701c367e489
Alessandro Negrão <alessandro.negrao.s@gmail.com>
2020-09-25 00:40:13 -0400

    Added informations of model and Core Data

---
+++ CoreDataSample/Cell.swift
@@ -0,0 +1,23 @@
+//
+//  Cell.swift
+//  CoreDataSample
+//
+//  Created by Alessandro Negrão on 24/09/20.
+//
+
+import UIKit
+
+class BrinquedoTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CoreDataSample/ViewController.swift
+++ CoreDataSample/ViewController.swift
@@ -6,42 +6,82 @@
 //
 
 import UIKit
+import CoreData
+
+var database: [NSManagedObject] = []
 
 class ViewController: UIViewController {
     @IBOutlet weak var buttonAction: UIButton!
-    
+
     @IBAction func buttonActionTrue(_ sender: UIButton) {
         
-        let alert = UIAlertController(title: "Socorro", message: "O que aconteceu?", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString(key: "Whatever", comment: "default action"), style: .default, handler: {
-            _ in
-            NSLog(format: "The \"OK\" alert occured.")
+        let generator = UINotificationFeedbackGenerator()
+        //Avisa ao dispositivo para iniciar a parte mecanica
+        generator.prepare()
+        generator.notificationOccurred(.success)
+        
+        let alert = UIAlertController(title: "Alerta de Brinquedo!", message: "Hi Lorena", preferredStyle: .alert)
+        alert.addTextField()
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Save", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"save\" alert occured.")
+            let textField = alert.textFields?.first
+            save(nome:  (textField?.text)!)
         }))
         
-        
-        
-        alert.addTextField()
-        present(alert, animated: true)
-        
+
+        self.present(alert, animated: true, completion: nil)
     }
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let manage = appDelegate.persistentContainer.viewContext
+        let fetch = NSFetchRequest<NSManagedObject>(entityName: "Brinquedo")
+        
+        do {
+            database = try manage.fetch(fetch)
+            for brinquedo in database{
+                print("Registro: ", brinquedo.value(forKey: "nome") as! String)
+            }
+          } catch {
+            print("erro")
+          }
+    }
 }
+
+func save(nome: String){
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let manage = appDelegate.persistentContainer.viewContext
+    let entityBrinquedo = NSEntityDescription.entity(forEntityName: "Brinquedo", in: manage)!
+    let brinquedo = NSManagedObject(entity: entityBrinquedo, insertInto: manage)
+    brinquedo.setValue(nome, forKey: "nome")
+do{
+        try manage.save()
+        print("Deu bom")
+        database.append(brinquedo)
+        } catch {
+           print(error)
+        }
+}
+
+
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = database[indexPath.row].value(forKey: "nome") as? String
+        print("DATABASE: ", database[indexPath.row].value(forKey: "nome") as! String)
+
+        return cell!
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return database.count
     }
     
 }
